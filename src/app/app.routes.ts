d9aa0ee7d0eed5fed431874e3ec5ee465c5b6e32
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-06-11 22:14:15 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-
 import { LandinpageComponent } from './components/landinpage/landinpage.component';
 import { LoginComponent } from './components/login/login.component';
-
 import { PacienteComponent } from './components/paciente/paciente.component';
-
 import { HomeComponent } from './components/home/home.component';
-import { ListarsegurosComponent } from './components/paciente/seguro/listarseguros/listarseguros.component';
-import { CrearseguroComponent } from './components/paciente/seguro/crearseguro/crearseguro.component';
-import { SeguroComponent } from './components/paciente/seguro/seguro.component';
+import { ListaralergiasComponent } from './components/oncologo/listaralergias/listaralergias.component';
+import { oncologoComponent } from './components/oncologo/oncologo.component';
+import { CreaeditaalergiasComponent } from './components/oncologo/creaeditaalergias/creaeditaalergias.component';
+import { ListarcirugiasComponent } from './components/paciente/listarcirugias/listarcirugias.component';
+import { CreaeditacirugiasComponent } from './components/paciente/creaeditacirugias/creaeditacirugias.component';
 
 export const routes: Routes = [
  
@@ -26,19 +25,27 @@ export const routes: Routes = [
     path: 'home',component: HomeComponent
     
   },
-
   {
-    path:'seguros',component:SeguroComponent,
-    children:[
-            {path:'nuevo',component:CrearseguroComponent},
-            {path:'ediciones/:id',component:CrearseguroComponent}
-                   
-    ]
-
-}
-
-
-
-
+    path:'alergias',component:ListaralergiasComponent,
     
+  },
+  {
+    path:'alergias',component:oncologoComponent,
+    children:[
+        {path:'nuevo',component: CreaeditaalergiasComponent},
+        {path:'ediciones/:id',component:CreaeditaalergiasComponent}
+    ]
+  },
+  {
+    path:'cirugias',component: ListarcirugiasComponent, 
+  },
+  {
+    path:'cirugias',component: PacienteComponent, 
+    children:[
+        {path:'nuevo', component:CreaeditacirugiasComponent},
+        {path:'ediciones/:id',component:CreaeditacirugiasComponent}
+        
+    ]
+  }
+
 ];
