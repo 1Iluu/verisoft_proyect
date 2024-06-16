660fccf3639a4d594d80330c48fc4921c7b91102
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-06-16 12:26:48 -0500

    Create respuesta.ts

---
+++ src/app/models/respuesta.ts
@@ -0,0 +1,9 @@
+
+import {Pacient} from "./paciente"
+
+export class  Respuesta{
+    idRespuesta: number=0
+    gradoConsulta:string=""
+    nivelRespuesta:string=""
+    paciente: Pacient= new Pacient
+}
