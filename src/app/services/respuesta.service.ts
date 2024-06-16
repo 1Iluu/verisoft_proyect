917eb8377a61197fa73ffbd46b46fe4312b8b53b
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-06-16 12:26:36 -0500

    Create respuesta.service.ts

---
+++ src/app/services/respuesta.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/enviroment';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Respuesta } from '../models/respuesta';
+const base_url= environment.csbase;
+@Injectable({
+  providedIn: 'root'
+})
+export class respuestaService {
+  private url=`${base_url}/respuestas`;
+  private listaCambio= new Subject<Respuesta[]>();
+  constructor(private httpClient: HttpClient){}
+  list() {
+    return this.httpClient.get<Respuesta[]>(this.url);
+  }
+  inser(r: Respuesta) {
+    return this.httpClient.post(this.url, r);
+  }
+  setList(listaNueva: Respuesta[]) {
+    this.listaCambio.next(listaNueva);
+  }
+  getList() {
+    return this.listaCambio.asObservable();
+  }
+  listId(id: number) {
+    return this.httpClient.get<Respuesta>(`${this.url}/${id}`);
+  }
+  update(a: Respuesta) {
+    return this.httpClient.put(this.url, a);
+  }
+  delete(id: number) {
+    return this.httpClient.delete(`${this.url}/${id}`);
+  }
+  
+}
