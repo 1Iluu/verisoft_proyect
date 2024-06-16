40cffe51c3dd95e004528630ce25628601b967ad
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-06-16 12:27:45 -0500

    Update landinpage.component.ts

--- src/app/components/landinpage/landinpage.component.ts
+++ src/app/components/landinpage/landinpage.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListarsegurosComponent } from '../paciente/seguro/listarseguros/listarseguros.component';
+import { ListarcirugiasComponent } from '../paciente/cirguias/listarcirugias/listarcirugias.component';
 
 
 @Component({
   selector: 'app-landinpage',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,ListarsegurosComponent],
+  imports: [RouterOutlet,RouterLink,ListarsegurosComponent,ListarcirugiasComponent],
   templateUrl: './landinpage.component.html',
   styleUrl:    './landinpage.component.css'
 })
