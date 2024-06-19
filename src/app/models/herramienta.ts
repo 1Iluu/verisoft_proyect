f4a238aa89532824f89be10baa95cb62b78ec5ce
1Iluu <iguana29x@gmail.com>
2024-06-18 22:59:43 -0500

    commit

--- src/app/models/herramienta.ts
+++ src/app/models/herramienta.ts
@@ -2,6 +2,6 @@ import { Oncologo } from "./oncologo";
 
 export class Herramienta{
     herramienta_id:number=0;
-    oncologo:Oncologo=new Oncologo()
+    oncologo:Oncologo=new Oncologo();
     nombreHerramienta:string="";
 }
