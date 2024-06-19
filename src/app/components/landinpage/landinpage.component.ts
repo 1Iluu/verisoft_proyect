5caf8750479977625f82bd687e51225cca102d3b
1Iluu <iguana29x@gmail.com>
2024-06-19 10:05:32 -0500

    modificando landing

--- src/app/components/landinpage/landinpage.component.ts
+++ src/app/components/landinpage/landinpage.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListarsegurosComponent } from '../paciente/seguro/listarseguros/listarseguros.component';
 import { ListarcirugiasComponent } from '../paciente/cirguias/listarcirugias/listarcirugias.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-landinpage',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,ListarsegurosComponent,ListarcirugiasComponent],
+  imports: [RouterOutlet,RouterLink,ListarsegurosComponent,ListarcirugiasComponent,MatButtonModule],
   templateUrl: './landinpage.component.html',
   styleUrl:    './landinpage.component.css'
 })
