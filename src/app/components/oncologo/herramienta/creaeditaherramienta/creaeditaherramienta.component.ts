a4dec4218eca62e44fe87a8f15497ec954dcca3c
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-20 01:00:37 -0500

    delete

--- src/app/components/oncologo/herramienta/creaeditaherramienta/creaeditaherramienta.component.ts
+++ src/app/components/oncologo/herramienta/creaeditaherramienta/creaeditaherramienta.component.ts
@@ -1,33 +1,45 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+
 import { Herramienta } from '../../../../models/herramienta';
 import { ServicioherramientaService } from '../../../../services/servicioherramienta.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Oncologo } from '../../../../models/oncologo';
+import { OncologoService } from '../../../../services/oncologo.service';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-creaeditaherramienta',
   standalone: true,
   imports: [
     MatFormFieldModule,
-     ReactiveFormsModule,
-     MatSelectModule,
-     CommonModule,
-     MatInputModule,
-     MatButtonModule,
+    NgIf,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './creaeditaherramienta.component.html',
   styleUrl: './creaeditaherramienta.component.css'
 })
-export class CreaeditaherramientaComponent implements OnInit{
+export class CreaeditaherramientaComponent implements OnInit {
 form:FormGroup=new FormGroup({});
 service:Herramienta=new Herramienta();
-id:number=0;
-edicion:boolean=false
+mensaje: string = '';
+id: number = 0;
+edicion: boolean = false;
 
 listaHerramientas:{value:string; viewValue:string}[]=[
   {value:'Bisturí',viewValue:'Bisturí'},
@@ -44,67 +56,59 @@ listaHerramientas:{value:string; viewValue:string}[]=[
   {value:'Agujas de Biopsia',viewValue:'Agujas de Biopsia'},
   {value:'Laser Quirúrgico',viewValue:'Laser Quirúrgico'},
   {value:'Máquina Gamma Knife',viewValue:'Máquina Gamma Knife'},
-  {value:'Máquina CyberKnife',viewValue:'CyberKnife'}
-];
+  {value:'Máquina CyberKnife',viewValue:'CyberKnife'},]
 
 constructor(
   private formBuilber:FormBuilder,
   private hS:ServicioherramientaService,
   private router:Router,
-  private route:ActivatedRoute
+  private route:ActivatedRoute,
+  private os: OncologoService
 ){}
 
 ngOnInit(): void{
+  this.route.params.subscribe((data: Params) => {
+    this.id = data['id'];
+    this.edicion = data['id'] != null;
+    this.init();
+  });
 
-  this.route.params.subscribe((data:Params)=>{
-  this.id=data['id'];
-  this.edicion=data['id']!=null;
-  this.init() 
- })
-
-  this.form=this.formBuilber.group({
-    codigo_herramienta: ['',Validators.required],
-
-    codigo_oncologo: ['', Validators.required],
-
-    nombreHerramienta:['',Validators.required],
-})
+  this.form = this.formBuilber.group({
+    herramienta_id: ['', Validators.required],
+    oncologo_id: ['', Validators.required],
+    nombreHerramienta: ['', Validators.required],
+ });
 }
-
 guardar(): void{
 if (this.form.valid) {
-  this.service.herramienta_id=this.form.value.codigo_herramienta;
-  this.service.oncologo.oncologo_id=this.form.value.oncologo.codigo_oncologo;
+  this.service.herramienta_id=this.form.value.herramienta_id;
+  this.service.oncologo.oncologo_id=this.form.value.oncologo_id;
   this.service.nombreHerramienta=this.form.value.nombreHerramienta;
-  console.log(this.service);
-
-  if(this.edicion){
-    this.hS.update(this.service).subscribe((data) => {
-      this.hS.list().subscribe((data) => {
-        this.hS.setList(data);
+    if (this.edicion) {
+      this.hS.update(this.service).subscribe(() => {
+        this.hS.list().subscribe((data) => {
+          this.hS.setList(data);
+        });
       });
-    });
-  }else{
-
-    this.hS.insert(this.service).subscribe((data) => {
-      this.hS.list().subscribe((data) => {
-        this.hS.setList(data);
+    } else {
+      this.hS.insert(this.service).subscribe((data) => {
+        this.hS.list().subscribe((data) => {
+          this.hS.setList(data);
+        });
       });
-    });
+    }
     this.router.navigate(['herramientas']);
-  };
- }
-}
-
-init(){
-  if(this.edicion){
-    this.hS.ListId(this.id).subscribe((data) => {
-      this.form=new FormGroup({
-        codigo_herramienta:new FormControl(data.herramienta_id),
-        codigo_oncologo:new FormControl(data.oncologo.oncologo_id),
-        nombreHerramienta:new FormControl(data.nombreHerramienta),
-      });
-    })
+    }
   }
-}
+  init() {
+    if (this.edicion) {
+      this.hS.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          herramienta_id: new FormControl(data.herramienta_id),
+          oncologo_id: new FormControl(data.oncologo.oncologo_id),
+          nombreHerramienta: new FormControl(data.nombreHerramienta),
+        });
+      });
+    }
+  }
 }
