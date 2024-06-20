d3dbb0fe2ad6e2b143b29498c42e0d7954f955fb
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-19 23:37:16 -0500

    corrijo

--- src/app/components/examendeteccion/listarexamen/listarexamen.component.ts
+++ src/app/components/examendeteccion/listarexamen/listarexamen.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {RouterLink} from '@angular/router';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { Examendeteccion } from '../../../models/examendeteccion';
 import { Serviceexamen } from '../../../services/serviceexamen.service';
 
@@ -14,12 +15,14 @@ import { Serviceexamen } from '../../../services/serviceexamen.service';
     MatTableModule,
     MatIconModule,
     MatButtonModule,
+    MatPaginatorModule,
     RouterLink
   ],
   templateUrl: './listarexamen.component.html',
   styleUrl: './listarexamen.component.css'
 })
 export class ListarexamenComponent implements OnInit { 
+  dataSource: MatTableDataSource<Examendeteccion> = new MatTableDataSource();
   displayedColumns: string[] = [
     'codigo',
     'fecha',
@@ -29,15 +32,20 @@ export class ListarexamenComponent implements OnInit {
     'accion01'
   ];
 
-  dataSource:MatTableDataSource<Examendeteccion>=new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private eS:Serviceexamen
   ){}
   ngOnInit(): void {
-    this.list();
-
+    this.eS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.eS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
   }
-
   list(){
     this.eS.list().subscribe((data)=>{
       this.dataSource=new MatTableDataSource(data);
