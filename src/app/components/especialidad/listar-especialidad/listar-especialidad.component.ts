95b88bb4fbb8d5eb27370e9fca378d4d1d0e031f
havuh <u20211c710@upc.edu.pe>
2024-06-22 15:13:07 -0500

    actualizado estados de tratamientos

--- src/app/components/especialidad/listar-especialidad/listar-especialidad.component.ts
+++ src/app/components/especialidad/listar-especialidad/listar-especialidad.component.ts
@@ -3,41 +3,56 @@ import { Especialidad } from './../../../models/especialidad';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-listar-especialidad',
   standalone: true,
-  imports: [MatTableModule,RouterLink,MatButtonModule],
+  imports: [MatTableModule, RouterLink, MatButtonModule],
   templateUrl: './listar-especialidad.component.html',
-  styleUrl: './listar-especialidad.component.css'
+  styleUrl: './listar-especialidad.component.css',
 })
 export class ListarEspecialidadComponent implements OnInit {
-  displayedColumns: string[] =
-  [
-  'id',
-  'nombreEspecialidad',
-  'complejidad',
-  'accion01',
-  'accion02'
+  displayedColumns: string[] = [
+    'id',
+    'nombreEspecialidad',
+    'complejidad',
+    'accion01',
+    'accion02',
   ];
 
-  dataSource:MatTableDataSource<Especialidad> = new MatTableDataSource()
+  dataSource: MatTableDataSource<Especialidad> = new MatTableDataSource();
 
-  constructor(private sS:EspecialidadService ){}
+  constructor(
+    private sS: EspecialidadService,
+    private _snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.sS.list().subscribe((data)=>{
-      this.dataSource=new MatTableDataSource(data)
-    })
-    this.sS.getList().subscribe((data)=>{
-      this.dataSource=new MatTableDataSource(data)
-    })
+    this.sS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+    this.sS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
   }
-  deletes(id:number){
-    this.sS.delete(id).subscribe((data)=>{
-      this.sS.list().subscribe((data)=>{
-        this.sS.setList(data)
-      })
-    })
+  deletes(id: number) {
+    this.sS.delete(id).subscribe({
+      next: (data) => {
+        this.sS.list().subscribe((data) => {
+          this.sS.setList(data);
+        });
+      },
+      error: (error) => {
+        this._snackbar.open(
+          'Primero elimine los oncologos vinculados a esta especialidad',
+          'Cerrar',
+          {
+            duration: 5000,
+          }
+        );
+      },
+    });
   }
 }
