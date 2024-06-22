48ca8999f82442b0f42974fe9c531b8ae16f1309
Victor Alejandro Arévalo Díaz <u202112543@upc.edu.pe>
2024-06-22 16:58:54 -0500

    ARREGLO ONCOLOGO
    QUITE EDUCACION, Y ARREGLE NOMBREYAPELLIDO

--- src/app/components/oncologo/listaroncologo/listaroncologo.component.ts
+++ src/app/components/oncologo/listaroncologo/listaroncologo.component.ts
@@ -35,7 +35,17 @@ export class ListaroncologoComponent implements OnInit{
   'salario',
   'nombreyapellido',
   'accion01',
-  'accion02',
+  'accion02'
+
+  /*  oncologo_id: number = 0;
+  user_id: Users = new Users();
+  especialidad_id: Especialidad = new Especialidad();
+  experiencia_laboral_anios: number=0;
+  cantidad_pacientes: number=0;
+  edad: number=0;
+  horario_atencion: string="";
+  salario: number =0;
+  nombreyapellido: string =""; */
   ]
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   constructor(private oS: OncologoService) {}
