2f8f1ec97feb02af5b8a30c5197c0f3913c9fe67
kyaraabrigo <kyaraabrigo2011@gmail.com>
2024-06-25 08:25:00 -0500

    graficoreport2

--- src/app/components/reportsaf/report-abrigo02/report-abrigo02.component.ts
+++ src/app/components/reportsaf/report-abrigo02/report-abrigo02.component.ts
@@ -10,6 +10,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { BaseChartDirective } from 'ng2-charts';
 import { Respuesta } from '../../../models/respuesta';
 import { respuestaService } from '../../../services/respuesta.service';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-report02',
@@ -28,7 +29,19 @@ import { respuestaService } from '../../../services/respuesta.service';
   styleUrl: './report-abrigo02.component.css'
 })
 export class reportAbrigo02  implements OnInit {
-   
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabel:string[] = [];
+  barChartType:ChartType = 'bar'
+  barChartLegend=true;
+  barChartData:ChartDataset[] = [];
+  barChartLabels: string[] = [];
+  //barChartType: ChartType = 'pie';
+  //barChartType: ChartType = 'line';
+  //barChartType: ChartType = 'polarArea';
+
+  form:FormGroup=new FormGroup({});
   displayedColumns: string[]=
   [
     'idRespuesta',
@@ -40,7 +53,6 @@ export class reportAbrigo02  implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   dataSource: MatTableDataSource<Respuesta> = new MatTableDataSource();
 
-  form:FormGroup=new FormGroup({});
   constructor(private rS: respuestaService,
     private formBuilder:FormBuilder) {
  
@@ -59,9 +71,17 @@ export class reportAbrigo02  implements OnInit {
       this.rS.getQuantityGrado(this.form.value.grado).subscribe(data => {
         this.dataSource=new MatTableDataSource(data);
         this.dataSource.paginator = this.paginator;
-     
+        this.barChartLabel = data.map(item => "Cantidad");
+        this.barChartData = [{
+          data: data.map(item => data.length),
+          label:'Respuestas por Grado',
+          backgroundColor: ['#DCDCB8'],
+          barThickness:150
+        }] 
       });
       
       }
   }
 }
+
+
